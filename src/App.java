01cb264f8d4a29c7e9d39378ce0e7c5a6545f8fb
Yuvi-ka26 <134833341+Yuvi-ka26@users.noreply.github.com>
2023-07-20 19:55:55 +0530

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args){
+        //initiate the GUI
+        PasswordGeneratorGUI PasswordGeneratorGUI = new PasswordGeneratorGUI();
+
+        //make the GUI visible
+        PasswordGeneratorGUI.setVisible(true);
+    }
+}
